08cb047466312d4271aa3c5b8135ec78a3aff69d
moleNULL <94998782+moleNULL@users.noreply.github.com>
2022-11-09 16:04:32 +0200

    Add StyleCop with settings.ruleset

--- LoggerWtihExceptions/Program.cs
+++ LoggerWtihExceptions/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace LoggerWtihExceptions
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine(" ");
         }
